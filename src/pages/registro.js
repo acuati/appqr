12a31c3cd1fa12adf55eca97680cd3429fc617cf
acuati <benalibenaliabdelkbir@gmail.com>
2024-06-19 13:04:27 +0200

    actualizo cambios

--- src/pages/registro.js
+++ src/pages/registro.js
@@ -4,57 +4,63 @@ import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
 import CollapseRedes from '../components/collapseredes';
 import ButtonAcceso from '../components/buttonacceso';
 import ButtonSegacess from '../components/buttonsegacess';
 const Registro = () => (
   <Layout>
    
-    <div className="">
+   <h2>Registro de usuarios</h2>
         <form className="login-form">
-          <h2>Registro de usuarios</h2>
-
-        
-          <label htmlFor="username">Nombre:</label>
-          <div>
-          <input type="text" id="username" name="username" required />
-          </div>
-
-          <label htmlFor="username">Delegación:</label>
-          
-          <div>
-          <input type="text" id="username" name="username" required />
-          </div>
-          
-          <label htmlFor="username">Correo electrónico:</label>
-          <div>
-          <input type="email" id="username" name="username" required />
-          </div>
-          
-
+          <label htmlFor="username">* Nombre:</label>
          
-          <label htmlFor="username">Confirmar correo electrónico:</label>
+          <input type="text" id="username" name="username" required />
           
-          <div>
-          <input type="email" id="username" name="username" required />
-          </div>
-
-          <label htmlFor="username">Contraseña:</label>
-          <div>
-          <input type="password" id="username" name="username" required />
-          </div>
-
-          <label htmlFor="username">Confirmar contraseña:</label>
-          <div>
-          <input type="password" id="username" name="username" required />
-          </div>
-
-          <button type="submit">Ingresar</button>
         </form>
+        <form className="login-form">
+        <label htmlFor="username">*  Delegación:</label>
+       
+        <input type="text" id="username" name="username" required />
+        
+        </form>
+        <form className="login-form">
+        <label htmlFor="username">* Correo electrónico:</label>
+      
+          
+          <input type="email" id="username" name="username" required />
+        
+        </form>
+        <form className="login-form">
+        <label htmlFor="username">* Confirmar correo electrónico:</label>
+        
+          
+          <input type="email" id="username" name="username" required />
+        
+        </form>
+        <form className="login-form">
+        <label htmlFor="username">* Contraseña:</label>
+        
+        <input type="password" id="username" name="username" required />
+        
+        </form>
+        <form className="login-form">
+          
+        <label htmlFor="username">* Confirmar contraseña:</label>
+        
+          
+          <input type="password" id="username" name="username" required />
+       
+        <button type="submit">Ingresar</button>
+        </form>
+         
+
+          
+        
 
 
 
-    </div>
+    
 
     <Link to="/login">Volver al login</Link>
 
